dcb2a6019d8eb4e9af46c6c39e745d540f571c5b
First007x <lesaen2005@gmail.com>
2025-10-08 09:59:56 +0700

    CI/CD

--- quasar.config.ts
+++ quasar.config.ts
@@ -39,7 +39,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/id377-suthiphong-leasan/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
